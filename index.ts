ad2e4b79f10ad311e79dcba648c8911a34a81544
Alpiny <154293262+Alpinixx@users.noreply.github.com>
2024-08-24 02:52:25 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -98,7 +98,7 @@ async function decodeInputData(
         transaction.data
       );
       if (
-        functionName == "swapExactTokensForTokens" ||
+        functionName == "swapETHForExactTokens" ||
         functionName == "swapExactETHForTokens" ||
         functionName == "swapExactETHForTokensSupportingFeeOnTransferTokens"
       ) {
@@ -127,41 +127,6 @@ async function checkPendingTransactions() {
     "pending",
     true,
   ]);
-  // Comment this part for existing tx
-  // const txhash =
-  //   // "0xceb1183378210b5d447bef1250f36b3e1087e706fc104e72d49265f72df89625"; // Two tokens path
-  //   "0x2f6ff1f6230bff664cd5d3701611f61758fb482a295ec70ddb48ea706e50d3fa"; // Three tokens path
-  // const { swapType, path, amountIn } = await decodeInputData(txhash);
-  // if (path.length >= 2) {
-  //   const tokenA = await getTokenDetails(path[0]);
-  //   const tokenB = await getTokenDetails(path[path.length - 1]);
-  //   console.log(`- ${swapType}`);
-  //   console.log(
-  //     `- Swap: ${tokenA.symbol.toUpperCase()} -> ${tokenB.symbol.toUpperCase()}`
-  //   );
-  //   console.log(
-  //     `  |_${tokenA.tokenName}:\t$ ${tokenA.price}, Decimals: ${tokenA.decimals}`
-  //   );
-  //   console.log(
-  //     `  |_${tokenB.tokenName}:\t$ ${tokenB.price}, Decimals: ${tokenB.decimals}`
-  //   );
-  //   const amountInExact = Number(amountIn) / Math.pow(10, tokenA.decimals);
-  //   const poolRate = Number(tokenA.price / tokenB.price);
-
-  //   const gasPrice = ethers.formatUnits(transaction.gasPrice, "gwei");
-  //   const gasLimit = Number(transaction.gasLimit);
-  //   const gasFeeETH = gasPrice * gasLimit;
-  //   const ethPrice = await getTokenDetails(
-  //     "0x0000000000000000000000000000000000000000"
-  //   ); // Fetch ETH price from CoinGecko
-  //   const gasFeeUSD = gasFeeETH * ethPrice.price;
-
-  //   const amountOut = Number(amountInExact * poolRate - gasFeeUSD);
-  // } else {
-  //   console.log(`Transaction type: ${swapType}\nPath: Not a valid swap path`);
-  // }
-  // console.log(`Transaction Hash: https://etherscan.io/tx/${txhash}`);
-  // console.log("---------------------------------");
 
   // Comment this part for pending tx
   if (pendingTransactions && pendingTransactions.transactions) {
@@ -173,85 +138,51 @@ async function checkPendingTransactions() {
         pendingTransactionsTime.set(tx.hash, Date.now());
         processedTransactions.add(tx.hash);
 
+        const now = new Date();
+        const formattedUTC = now.toLocaleString(undefined, { timeZone: "UTC" });
+
         console.log(`💡 Gotcha!\n`);
+        console.log(formattedUTC);
+
         const { swapType, path, amountIn } = await decodeInputData(tx.hash);
         if (path.length >= 2) {
           const tokenA = await getTokenDetails(path[0]);
           const tokenB = await getTokenDetails(path[path.length - 1]);
-          console.log(`✅ ${swapType}`);
-          console.log(
-            `- Swap: ${tokenA.symbol.toUpperCase()} -> ${tokenB.symbol.toUpperCase()}`
-          );
-          console.log(
-            `  |_${tokenA.tokenName}:\t$ ${tokenA.price?.toLocaleString(
-              "en-US"
-            )}, ` +
-              `Supply: ${tokenA.supply.toLocaleString("en-US")}\t(${
-                tokenA.decimals
-              })`
-          );
-          console.log(
-            `  |_${tokenB.tokenName}:\t$ ${tokenB.price?.toLocaleString(
-              "en-US"
-            )}, ` +
-              `Supply: ${tokenB.supply.toLocaleString("en-US")}\t(${
-                tokenB.decimals
-              })`
-          );
-          const amountInExact = (tx.value = 0
-            ? Number(amountIn) / Math.pow(10, Number(tokenA.decimals))
-            : tx.vlaue);
+          const amountInExact =
+            tx.value == 0
+              ? Number(amountIn) / Math.pow(10, Number(tokenA.decimals))
+              : tx.vlaue;
           const poolRate = Number(tokenA.price) / Number(tokenB.price);
-          console.log(
-            `Amount In:\t${amountInExact.toLocaleString(
-              "en-US"
-            )}\nPool Rate:\t${poolRate}`
-          );
-          // const gasPrice = Number(ethers.formatUnits(tx.gasPrice, "gwei"));
-          // const gasLimit = Number(tx.gasLimit);
+          const amountOut = Number(amountInExact) * Number(poolRate);
           const gasFeeETH =
-            Number(ethers.formatUnits(tx.gasPrice, "gwei")) *
+            Number(ethers.formatUnits(tx.gasPrice, "ether")) *
             Number(tx.gasLimit);
           const wETH = await getTokenDetails(
             "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
           );
           const gasFeeUSD = gasFeeETH * Number(wETH.price);
-
-          const amountOut = Number(amountInExact * poolRate - gasFeeUSD);
-          console.log(`Amount Out:\t${amountOut}`);
+          console.log(`✅ ${swapType}`);
+          console.log(`From: ${tx.from}`);
+          console.log(
+            `- Swap: ${amountInExact} (${tokenA.symbol.toUpperCase()}) -> ` +
+              `${amountOut} (${tokenB.symbol.toUpperCase()})`
+          );
+          console.log(
+            `  |_${tokenA.tokenName}:\tCurrent Price: $${tokenA.price}, ` +
+              `Supply: ${tokenA.supply}\t`
+          );
+          console.log(
+            `  |_${tokenB.tokenName}:\tCurrent Price: $${tokenB.price}, ` +
+              `Supply: ${tokenB.supply}\t`
+          );
+          console.log(`Gas Fee: $${gasFeeUSD.toFixed(2)}
+          )}`);
+          // console.log(`Value: ${ethers.formatEther(tx.value)} ETH`);
         } else {
           console.log(`🚫 ${swapType}`);
         }
-        console.log(`Value: ${ethers.formatEther(tx.value)} ETH`);
         console.log(`🔗 Tx: https://etherscan.io/tx/${tx.hash}`);
-        console.log("---------------------------------");
-        const checkReceipt = async () => {
-          const receipt = await provider.getTransactionReceipt(tx.hash);
-          if (receipt) {
-            const pendingTime =
-              Date.now() - pendingTransactionsTime.get(tx.hash)!;
-            console.log(
-              `\n🕒 Transaction was pending for ${(pendingTime / 1000).toFixed(
-                2
-              )} seconds`
-            );
-            console.log(
-              `Transaction Status: ${
-                receipt.status === 1 ? "Success" : "Failed"
-              }`
-            );
-            console.log(`Gas Used: ${receipt.gasUsed.toString()}`);
-
-            if (receipt.logs.length > 0) {
-              receipt.logs.forEach((log) => {});
-            }
-            console.log("====================================================");
-          } else {
-            setTimeout(checkReceipt, 1000);
-          }
-        };
-
-        checkReceipt();
+        console.log("---------------------------------\n");
       }
     }
   }
