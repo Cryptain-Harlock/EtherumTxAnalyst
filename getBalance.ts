97b58324046c9c2eb6c76666ac58fe3e7386aec9
Harlock <154293262+Cryptain-Harlock@users.noreply.github.com>
2024-10-17 11:40:18 +0900

    Update getBalance.ts

--- getBalance.ts
+++ getBalance.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import { monitoredAddresses } from "./monitoredAddresses";
 
 const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
@@ -23,15 +24,4 @@ async function getBalance(addresses) {
   }
 }
 
-const monitoredAddress = [
-  "0xae2Fc483527B8EF99EB5D9B44875F005ba1FaE13",
-  "0xb0ba33566bd35bcb80738810b2868dc1ddd1f0e9",
-  "0x3b40af8e80b09f4a54b1eb763031d4880f765bdc",
-  "0xab7b44ae25af88d306dc0a5c6c39bbeb8916eabb",
-  "0xacbcb2724cfafb839c752d71997a8a7a16989b2e",
-  "0x49c543e8873aeda1b60c176f55a78fc62f9c9fbb",
-  "0x3ccce09b4ad94968f269375c0999134a6617f795",
-  "0x16d59f67bd39ac0d952e48648548217b62183403",
-].map((address) => address.toLowerCase());
-
-getBalance(monitoredAddress).catch(console.error);
+getBalance(monitoredAddresses).catch(console.error);
